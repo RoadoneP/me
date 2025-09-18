dd1477c83f15c81b1ec09f841d16e9c08aca6754
RoadoneP <71438979+RoadoneP@users.noreply.github.com>
2025-09-18 22:27:57 +0900

    back

--- next.config.js
+++ next.config.js
@@ -4,10 +4,6 @@ const withMDX = require("@next/mdx")();
 const nextConfig = {
   reactStrictMode: true,
   pageExtensions: ["js", "jsx", "mdx", "ts", "tsx"],
-  i18n: {
-    locales: ["en", "ko"], // 지원하는 언어들
-    defaultLocale: "en",   // 기본 언어
-  },
 };
 
 module.exports = withMDX(nextConfig);
